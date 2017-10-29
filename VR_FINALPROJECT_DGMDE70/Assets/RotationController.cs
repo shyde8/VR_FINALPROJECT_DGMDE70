91d45c4ce72de4f3005a9b42aabce2ebfbe5d9ca
Golfire <megaman.t@gmail.com>
2017-10-29 17:09:46 -0400

    Graphics for UI and Compass added
    Graphics for UI and Compass added

---
+++ VR_FINALPROJECT_DGMDE70/Assets/RotationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject rotateOverY;	
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = rotateOverY.transform.rotation;
+	}
+}
