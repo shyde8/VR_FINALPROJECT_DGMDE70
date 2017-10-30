efa7535dfbb59c0fc97b02f918e72bf82e86df31
Golfire <megaman.t@gmail.com>
2017-10-29 21:38:47 -0400

    Added diamond  pointer
    Added diamond  pointer

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/DiamondRotationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondRotationController : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(AnimateDiamond());
+	}
+
+    IEnumerator AnimateDiamond() {
+        transform.Rotate(new Vector3(0, 1, 0));
+        yield return new WaitForSeconds(1);
+
+    }
+}
