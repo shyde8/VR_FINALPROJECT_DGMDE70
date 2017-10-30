c8fbe9a867fcb2217636095ab13fbd3634d8f759
Golfire <megaman.t@gmail.com>
2017-10-29 20:51:42 -0400

    Door system

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/DoorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject positionTarget;
+
+    [SerializeField]
+    private float speed = 5;
+
+    private void Update() {
+        OpenDoor();
+    }
+
+    public void OpenDoor() {
+        transform.position = Vector3.Lerp(transform.position, positionTarget.transform.position, Time.deltaTime);
+    }
+}
