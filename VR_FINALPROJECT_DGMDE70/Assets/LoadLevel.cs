d89d0e34727e6ad091eda5f5bf6c7f864dafed0e
Golfire <megaman.t@gmail.com>
2017-11-07 16:37:22 -0500

    Test for level loading
    Level loading test

---
+++ VR_FINALPROJECT_DGMDE70/Assets/LoadLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+    [SerializeField]
+    private string sceneName;
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("LOAD");
+
+        //Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(sceneName);
+        
+        //AssetBundle loadedAssets = AssetBundle.LoadFromFile("Assets/scenes/" + sceneName);
+        //string[] scenePaths = loadedAssets.GetAllScenePaths();
+    }
+}
