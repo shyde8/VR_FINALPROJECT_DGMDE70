f3c0d680577fe88bb39b820e350c84eb7429577d
Golfire <megaman.t@gmail.com>
2017-11-08 00:00:07 -0500

    Test of multi level
    Test of multi level

--- VR_FINALPROJECT_DGMDE70/Assets/LoadLevel.cs
+++ VR_FINALPROJECT_DGMDE70/Assets/LoadLevel.cs
@@ -8,13 +8,7 @@ public class LoadLevel : MonoBehaviour {
     [SerializeField]
     private string sceneName;
 
-    private void OnTriggerEnter(Collider other) {
-        Debug.Log("LOAD");
-
-        //Application.LoadLevel(sceneName);
-        SceneManager.LoadScene(sceneName);
-        
-        //AssetBundle loadedAssets = AssetBundle.LoadFromFile("Assets/scenes/" + sceneName);
-        //string[] scenePaths = loadedAssets.GetAllScenePaths();
+    private void OnTriggerEnter(Collider other) {              
+        SceneManager.LoadScene(sceneName);              
     }
 }
