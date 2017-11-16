68d3a0f412fab94bcd2c4ddf0f5c8b73e0e1cd24
Golfire <megaman.t@gmail.com>
2017-11-16 16:49:40 -0500

    Music BG

---
+++ VR_FINALPROJECT_DGMDE70/Assets/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+
+    private Animator anim;
+    private NavMeshAgent agent;
+    
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(player.transform.position);
+	}
+}
