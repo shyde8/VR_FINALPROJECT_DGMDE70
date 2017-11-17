9ea4935df75bbb6a7e96d82387b90fcef364b8fc
carlitoselbuho <33007503+carlitoselbuho@users.noreply.github.com>
2017-11-16 22:05:30 -0500

    carlos-mybox

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/barrelexplotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrelexplotion : MonoBehaviour {
+	ParticleSystem myexplotion;
+	Collider mycollider;
+	// Use this for initialization
+	void Start () {
+		mycollider = GetComponent<Collider>();
+		mycollider.isTrigger = false;
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(mycollider.isTrigger == true){
+			awake();
+
+			Destroy(gameObject, 2);
+		}
+		
+	}
+
+	void awake(){
+		myexplotion = GetComponentInChildren<ParticleSystem>();
+		myexplotion.Play();
+	}
+	
+}
