a801c6ec32c61f4b612ed47a75999a812df8793c
shyde8 <32993251+shyde8@users.noreply.github.com>
2017-11-26 21:18:31 -0500

    Hyde- added health bar
    Hyde- added health bar

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/Weapon/HealthBar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour 
+{
+	public Image CurrentHealthBar;
+	public Text ratioText;
+
+	private float hitPoint = 100;
+	private float maxHitPoint = 100;
+
+	private void Start()
+	{
+		//Calling UpdateHealthBar() to start
+		UpdateHealthBar();
+
+
+	}
+
+	private void UpdateHealthBar()
+	{
+		//gives us value between 0 and 1
+		float ratio = hitPoint / maxHitPoint;
+
+		//scale CurrentHealthBar based on ratio
+		CurrentHealthBar.rectTransform.localScale = new Vector3 (ratio, 1, 1);
+
+		ratioText.text = (ratio * 100).ToString("0") + '%';
+
+
+	}
+
+	private void TakeDamage(float damage)
+	{
+		hitPoint -= damage;
+
+		//placeholder to prevent scaling issues until we come up with GameOver scenario
+		if (hitPoint < 0) 
+		{
+			hitPoint = 0;
+		}
+
+		UpdateHealthBar();
+	}
+
+	private void HealDamage(float heal)
+	{
+		hitPoint += heal;
+
+		//placeholder
+		if (hitPoint > maxHitPoint) 
+		{
+			hitPoint = maxHitPoint;
+		}
+
+		UpdateHealthBar();
+	}
+}
