966179fc2a44a1dc85df9a421a5eb70fe07151e9
shyde8 <32993251+shyde8@users.noreply.github.com>
2017-11-26 21:57:34 -0500

    Hyde- working on "Damage Controller" script
    Hyde- working on "Damage Controller" script

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/Weapon/DamageController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour 
+{
+	public bool isDamaging;
+	public float damage = 10;
+
+	private void OnTriggerStay(Collider col)
+	{
+		if (col.tag == "PVR")
+		{
+			col.SendMessage((isDamaging)?"TakeDamage":"HealDamage",Time.deltaTime*damage);
+		}
+	}
+
+}
