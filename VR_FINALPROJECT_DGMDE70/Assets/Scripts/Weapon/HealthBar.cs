d432ed15cf1337cb8542ac1016c68b87081d3958
shyde8 <32993251+shyde8@users.noreply.github.com>
2017-11-28 21:06:45 -0500

    Hyde- implemented shield system
    Hyde- implemented shield system

--- VR_FINALPROJECT_DGMDE70/Assets/Scripts/Weapon/HealthBar.cs
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/Weapon/HealthBar.cs
@@ -6,35 +6,45 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour 
 {
 	public Image CurrentHealthBar;
+	public Image CurrentShieldBar;
 	public Text ratioText;
 
 	private float hitPoint = 100;
 	private float maxHitPoint = 100;
+	private float hitShield = 40;
+	private float maxHitShield = 100;
 
 	private void Start()
 	{
 		//Calling UpdateHealthBar() to start
 		UpdateHealthBar();
-
-
 	}
 
 	private void UpdateHealthBar()
 	{
 		//gives us value between 0 and 1
-		float ratio = hitPoint / maxHitPoint;
+		float healthRatio = hitPoint / maxHitPoint;
+		float shieldRatio = hitShield / maxHitShield;
 
 		//scale CurrentHealthBar based on ratio
-		CurrentHealthBar.rectTransform.localScale = new Vector3 (ratio, 1, 1);
+		CurrentHealthBar.rectTransform.localScale = new Vector3 (healthRatio, 1, 1);
+		CurrentShieldBar.rectTransform.localScale = new Vector3 (shieldRatio, 1, 1);
 
-		ratioText.text = (ratio * 100).ToString("0") + '%';
+		ratioText.text = ((healthRatio+shieldRatio) * 100).ToString("0") + '%';
 
 
 	}
 
 	private void TakeDamage(float damage)
 	{
-		hitPoint -= damage;
+		if (hitShield > 0) 
+		{
+			hitShield -= damage;
+		} 
+		else 
+		{
+			hitPoint -= damage;
+		}
 
 		//placeholder to prevent scaling issues until we come up with GameOver scenario
 		if (hitPoint < 0) 
@@ -47,12 +57,19 @@ public class HealthBar : MonoBehaviour
 
 	private void HealDamage(float heal)
 	{
-		hitPoint += heal;
-
-		//placeholder
-		if (hitPoint > maxHitPoint) 
+		if (hitPoint < maxHitPoint) 
 		{
-			hitPoint = maxHitPoint;
+			hitPoint += heal;
+		} 
+		else
+		{
+			hitShield += heal;
+		}
+
+		//placeholder to prevent scaling issues if we hit superceded maxHitShield
+		if (hitShield > maxHitShield) 
+		{
+			hitShield = maxHitShield;
 		}
 
 		UpdateHealthBar();
