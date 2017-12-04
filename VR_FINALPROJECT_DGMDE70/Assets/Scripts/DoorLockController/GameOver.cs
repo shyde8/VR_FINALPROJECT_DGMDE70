e4506d4bf6a3fda59cfd817243d7ce2f00365709
shyde8 <32993251+shyde8@users.noreply.github.com>
2017-12-03 20:13:58 -0500

    Hyde- added GameOver if health hits 0
    Hyde- added GameOver if health hits 0

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/DoorLockController/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour 
+{
+	bool gameHasEnded = false;
+	public float restartDelay = 1f;
+
+	public void endGame()
+	{
+		if (gameHasEnded == false) 
+		{
+			gameHasEnded = true;
+			Invoke ("restart", restartDelay);
+		}
+	}
+
+	void restart()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+}
