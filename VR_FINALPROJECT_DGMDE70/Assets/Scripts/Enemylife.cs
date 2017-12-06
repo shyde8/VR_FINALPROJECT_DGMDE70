a32272d8f7de3c59be21067807f4b6b0e5810af3
carlitoselbuho <33007503+carlitoselbuho@users.noreply.github.com>
2017-12-06 14:08:15 -0500

    carlos_two_scripts_monster

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/Enemylife.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemylife : MonoBehaviour {
+
+	Collider mycollider;
+	int life = 0;
+	// Use this for initialization
+	void Start () {
+		mycollider = GetComponent<Collider>();
+		mycollider.isTrigger = false;
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(mycollider.isTrigger == true){
+
+			//awake();
+			life = life + 1;
+			mycollider.isTrigger = false;
+			
+			if (life ==2){
+				Destroy(gameObject);
+			}
+		}
+		
+	}
+
+}
