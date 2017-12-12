320a55f568c05c7a0ef58bd791a579ccb1f7ab65
shyde8 <32993251+shyde8@users.noreply.github.com>
2017-12-11 21:04:48 -0500

    last door opens after collecting 4 keys
    last door opens after collecting 4 keys

---
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/DoorLockController/Controller_For_Second_Door_hyde.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_For_Second_Door_hyde : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (SpawnItemsBlue.hasKey==true && SpawnItemsRed.hasKey==true && SpawnItemsGreen.hasKey==true && SpawnItemsYellow.hasKey==true)
+		{
+			this.gameObject.GetComponent<DoorController>().OpenDoor();
+		}
+		
+	}
+}
