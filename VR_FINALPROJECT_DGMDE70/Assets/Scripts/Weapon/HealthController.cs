e0aaf7391222270ec8f340002c61cccb01beb802
Golfire <megaman.t@gmail.com>
2017-12-14 15:32:11 -0500

    Last Changes

--- VR_FINALPROJECT_DGMDE70/Assets/Scripts/Weapon/HealthController.cs
+++ VR_FINALPROJECT_DGMDE70/Assets/Scripts/Weapon/HealthController.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class HealthController : MonoBehaviour {
 
 	[SerializeField]private float health = 100;
-
+    [SerializeField]
+    private bool isBoss;
 	public void ApplyDamage(float damage)
 	{
 		health -= damage;
 
-		if (health <= 0f) 
-		{
+		if (health <= 0f){
+
+            if (isBoss) {
+                SceneManager.LoadScene("level_2");
+            }
 			Destroy (gameObject);
 		}
 	}
