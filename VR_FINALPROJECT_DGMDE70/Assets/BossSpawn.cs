d13f2f401ad44ae3e33148718b506f336724007b
Golfire <megaman.t@gmail.com>
2017-12-14 15:31:42 -0500

    last changes

---
+++ VR_FINALPROJECT_DGMDE70/Assets/BossSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawn : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject boss;
+
+    
+    private void OnTriggerEnter(Collider other) {
+        boss.SetActive(true);
+    }
+
+
+}
---
+++ VR_FINALPROJECT_DGMDE70/Assets/SpawnMonster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMonster : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject enemy, player;
+
+    public void Start() {
+        StartCoroutine(TriggerAlarm());
+    }
+
+    IEnumerator TriggerAlarm() {
+
+        while (true) {
+            GameObject ne = Instantiate(enemy);
+            ne.gameObject.transform.position = transform.position;
+            ne.GetComponent<EnemyController>().SetPlayer(player.gameObject);
+            yield return new WaitForSeconds(10);
+        }
+        
+        
+        
+    }
+}
